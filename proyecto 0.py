749713731f5a674875fca409fdc0155e7e1f1733
Daniel Otero <d-anican@hotmail.com>
2024-12-21 08:14:24 -0500

    se agrega el proyecto

---
+++ proyecto 0.py
@@ -0,0 +1,16 @@
+
+
+    def determinar_nivel_de_poder ():
+      nombre = input("GOKU")
+    
+    nivel_poder = random.randint(1000,100000)
+
+    print(f"¡Bienvenido, {GOKU}!")
+    print(f"Tu nivel de poder es {1000}.")
+
+    if nivel_poder > 9000:
+        print("¡Es más de 9000! ¡Eres un guerrero poderoso!")
+    else:
+        print("Tu nivel de poder es bajo. ¡Necesitas entrenar más!")
+   
+    determinar_nivel_de_poder()
